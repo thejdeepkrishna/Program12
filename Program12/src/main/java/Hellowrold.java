24d7b95b7166b36fb6b59b5a8b67551ff8a905dc
system administrator <system administrator@DESKTOP-FFNBBJ7>
2025-06-05 12:34:41 +0530

    initial commit

---
+++ Program12/src/main/java/Hellowrold.java
@@ -0,0 +1,12 @@
+
+public class Hellowrold {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =5;
+		int b = 31;
+		int c = a+b;
+		System.out.print("the value of c is:");
+		System.out.println(c);	}
+
+}
